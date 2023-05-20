df1bd381f11a8439d9733aa587822d8f26e7d4d2
Dhaivat Bhatt <Mdhaivat1994@gmail.com>
2023-05-20 22:50:16 +0000

    base class for detectron2

--- DWODLib/engine/engine.py
+++ DWODLib/engine/engine.py
@@ -5,6 +5,5 @@ from detectron2.engine import DefaultTrainer
 ## trainer class
 class Trainer(object):
     def __init__(self, cfg):
-        self.cfg = cfg
-        self.trainer = DefaultTrainer(cfg)
+        self.dt = DefaultTrainer(cfg) ## default trainer object
 
