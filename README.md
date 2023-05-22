ba0a273cc5bbcadb41ae1c477685fe1f76b06e02
Dhaivat Bhatt <Mdhaivat1994@gmail.com>
2023-05-22 02:21:06 +0000

    run inference on all images in a directory

---
+++ README.md
@@ -0,0 +1,18 @@
+# Instruction to run inference on new images:
+
+Requirement: Images you want to run inference on should be located in a particular directory. Path to where images should be stored should be provided too, the script will automatically create that new directory if it doesn't exist. 
+
+
+```
+cd ~/Dhiwise-OD
+source ./DhiwiseOD/bin/activate
+python3 tools/inference.py --options --inputImagesDir '/path/to/input/images' --modelPathDirectory 'experiments/R50_fasterRCNNv3/' --outputImageDir '/path/to/output/images'
+```
+
+## Example:
+
+All DhiWise data images are located in `/home/ubuntu/data_OD/images`. If you want to run inference on that amd save it to `/home/ubuntu/data_OD/inferenceResults`, you can run the following command. 
+
+```
+python3 tools/inference.py --options --inputImagesDir '/home/ubuntu/data_OD/images' --modelPathDirectory 'experiments/R50_fasterRCNNv3/' --outputImageDir '/home/ubuntu/data_OD/inferenceResults'
+```
---
+++ tools/inference.py
@@ -0,0 +1,78 @@
+"""
+   Inference on image directory using a trained model 
+"""
+
+
+### train object detection model with detectron2 and fiftyone
+
+## imports
+import os
+from tqdm import tqdm
+from DWODLib.config import defaultCfg as config
+from DWODLib.utils import (get_args, 
+                        load_json,
+                        build_detectron2_config,
+                        make_dir)
+
+from DWODLib.engine import Predictor
+import cv2
+import glob
+from detectron2.data import MetadataCatalog
+from detectron2.utils.visualizer import Visualizer
+
+## usage: python3 tools/inference.py --options --inputImagesDir '/path/to/input/images' --modelPathDirectory '/path/to/model/directory' --outputImageDir '/path/to/output/images'
+
+## get arguments
+args = get_args()
+
+## model directory has to be there
+assert args['modelPathDirectory'] is not None, "Model directory has to be specified for inference!"
+
+## output image directory has to be there
+assert args['outputImageDir'] is not None, "Output image directory has to be specified for storing images!"
+
+## input image directory has to be there
+assert args['inputImagesDir'] is not None, "Input image directory has to be specified for inference!"
+
+## make output directory
+make_dir(args['outputImageDir'])
+
+## override default config with new arguments
+config = load_json(os.path.join(args['modelPathDirectory'], 'experiment_config.json'))
+## Build dataset in fiftyone format
+class2Id = load_json(os.path.join(config['outputDir'], 'class2Id.json')) ## save mapping from class to id
+
+## detectron2 model
+det2Config = build_detectron2_config(config)
+det2Config.MODEL.WEIGHTS = os.path.join(config['outputDir'], 'model_final.pth') ## model weights
+det2Config.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args['testScoreThresh']  if 'testScoreThresh' in args else 0.7 ## set threshold for this model
+
+## Predictor object
+predictor = Predictor(det2Config)
+
+## list of images
+images = glob.glob(os.path.join(args['inputImagesDir'], '*.jpg'))
+
+## ensure that there are images in the input directory
+assert len(images) > 0, "No images found in the input directory!"
+
+## set classes metadata
+MetadataCatalog.get(det2Config.DATASETS.TRAIN[0]).set(thing_classes=list(class2Id.keys()))
+
+## run inference on each image
+for image in tqdm(images):
+    imageName = os.path.basename(image) ## name of the image
+    img = cv2.imread(image) ## read image
+
+    ## output image path
+    outputImagePath = os.path.join(args['outputImageDir'], imageName)
+
+    ## run inference
+    outputs = predictor.dp(img)
+
+    ## visualize
+    v = Visualizer(img[:, :, ::-1], metadata=MetadataCatalog.get(det2Config.DATASETS.TRAIN[0]), scale=1.0)
+    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+    ## save image
+    cv2.imwrite(outputImagePath, v.get_image()[:, :, ::-1])
